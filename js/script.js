bc50285328b617fa213c7a301a3a70d36151727b
MagdaIG <magda.inalaf@gmail.com>
2024-09-26 18:27:46 -0600

    arreglando calendario

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ document.querySelectorAll('.chat-list-item').forEach(item => {
         item.classList.remove('unread-message');
     });
 });
+
+// Colapsar el calendario en pantallas pequeñas
+document.addEventListener('DOMContentLoaded', function () {
+    const calendarCollapse = document.getElementById('calendarCollapse');
+
+    if (window.innerWidth <= 768) {
+        // Colapsar el calendario
+        const collapseInstance = new bootstrap.Collapse(calendarCollapse, {
+            toggle: false
+        });
+        collapseInstance.hide(); // Ocultar por defecto en pantallas pequeñas
+    }
+});
